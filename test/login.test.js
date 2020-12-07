c8fa0aa187cbcc6c3c1a4c96a8d4a8b4ed0ca103
J Rhee <jayrhee0514@gmail.com>
2020-12-07 13:03:48 -0500

    worked on login.test.js

--- test/login.test.js
+++ test/login.test.js
@@ -4,7 +4,7 @@ const Login = require("../public/js/login");
 describe("Login", () => {
     describe("Initialization", () => {
         it("should let you log in", function () {
-            
+            const login = new Login("")
         });
     })
 })
