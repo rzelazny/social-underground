1dc39606287d858ab10a8a4346221e093d9f853d
J Rhee <jayrhee0514@gmail.com>
2020-12-07 15:35:24 -0500

    tested login and sign up on postman and added to test folder

--- test/login.test.js
+++ test/login.test.js
@@ -1,24 +1,24 @@
-const { expect, it } = require("@jest/globals");
-const Login = require("../public/js/login");
+// const { expect, it } = require("@jest/globals");
+const loginUser = require("../public/js/login");
 
 describe("Login", () => {
     describe("Initialization", () => {
         it("should let you log in", function () {
-            const login = new Login("fake@gmail.com", "password");
+            const login = new loginUser("fake@gmail.com", "password");
 
-            expect(login.email).toEqual("fake@gmail.com");
-            expect(login.password).toEqual("password");
+            expect(login.emailInput).toEqual("fake@gmail.com");
+            expect(login.passwordInput).toEqual("password");
         });
 
         it("should throw an error if not provided an email", () => {
-            const cb = () => new Login ("fake@gmail.com");
+            const cb = () => new loginUser ("fake@gmail.com");
             const err = new Error ("Expected parameter 'name' to be a non-empty string");
 
             expect(cb).toThrowError(err);
         });
 
         it("should throw an error if not provided a password", () => {
-            const cb = () => new Login ("password");
+            const cb = () => new loginUser ("password");
             const err = new Error ("Expected parameter 'name' to be a non-empty string");
 
             expect(cb).toThrowError(err);
