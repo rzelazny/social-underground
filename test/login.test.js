c9778dc950180757f83e73744a0a513faf467d6a
J Rhee <jayrhee0514@gmail.com>
2020-12-07 13:47:54 -0500

    added more a throw error code to show any errors

--- test/login.test.js
+++ test/login.test.js
@@ -1,10 +1,27 @@
-const { iteratee } = require("lodash");
+const { expect, it } = require("@jest/globals");
 const Login = require("../public/js/login");
 
 describe("Login", () => {
     describe("Initialization", () => {
         it("should let you log in", function () {
-            const login = new Login("")
+            const login = new Login("fake@gmail.com", "password");
+
+            expect(login.email).toEqual("fake@gmail.com");
+            expect(login.password).toEqual("password");
         });
-    })
-})
+
+        it("should throw an error if not provided an email", () => {
+            const cb = () => new Login ("fake@gmail.com");
+            const err = new Error ("Expected parameter 'name' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });
+
+        it("should throw an error if not provided a password", () => {
+            const cb = () => new Login ("password");
+            const err = new Error ("Expected parameter 'name' to be a non-empty string");
+
+            expect(cb).toThrowError(err);
+        });
+    });
+});
