a768a89612ecc22788c280c4810c6b228a464bf9
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-07 13:53:36 -0500

    changed name of value to weight and deck array to let variable

--- games/blackjack/blackjack.js
+++ games/blackjack/blackjack.js
@@ -5,25 +5,26 @@
 const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
 const vals = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-var deck = [];
+let deck = [];
 
 function createDeck() {
     deck = [];
     for (var i = 0; i < vals.length; i++) {
         for (var j = 0; j < suits.length; j++) {
             if(vals[i] === "J" || vals[i] === "Q" || vals[i] === "K") {
-                value = 10;
+                weight = 10;
             }
             else if (vals[i] === "A") {
-                value = 11
+                weight = 11
             }
             else {
-                value = parseInt(vals[i])
+                weight = parseInt(vals[i])
             }
-            var card = { Value: vals[i], Suit: suits[j], Value: value };
+            var card = { Value: vals[i], Suit: suits[j], Weight: weight };
             deck.push(card);
         }
     }
 }
+
 createDeck();
 console.log(deck);
