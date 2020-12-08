d643b616fe3e117cec713cf034bc07cab4467329
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-08 12:36:16 -0500

    commented out unnecessary code for now // created array or objects to hold card data from api

--- games/blackjack/blackjack.js
+++ games/blackjack/blackjack.js
@@ -2,12 +2,12 @@
 //                Variables                  //
 ///////////////////////////////////////////////
 
-//https://deckofcardsapi.com/// look into tomo
+//https://deckofcardsapi.com/// 
 
-const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
-const vals = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+// const suits = ['clubs', 'diamonds', 'hearts', 'spades'];
+// const vals = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
 
-let deck = [];
+// let deck = [];
 
 let hand = [];
 
@@ -30,55 +30,71 @@ var ties = 0;
 //                Functions                  //
 ///////////////////////////////////////////////
 
-function createDeck() {
-    deck = [];
-    for (let i = 0; i < vals.length; i++) {
-        for (let j = 0; j < suits.length; j++) {
-            if (vals[i] === 'J' || vals[i] === 'Q' || vals[i] === 'K') {
-                weight = 10;
-            }
-            else if (vals[i] === 'A') {
-                weight = 11
-            }
-            else {
-                weight = parseInt(vals[i])
-            }
-            var card = { Value: vals[i], Suit: suits[j], Weight: weight };
-            deck.push(card);
-        }
-    }
-    return console.log(deck.length),
-    console.log(deck);
-}
+// function createDeck() {
+//     deck = [];
+//     for (let i = 0; i < vals.length; i++) {
+//         for (let j = 0; j < suits.length; j++) {
+//             if (vals[i] === 'J' || vals[i] === 'Q' || vals[i] === 'K') {
+//                 weight = 10;
+//             }
+//             else if (vals[i] === 'A') {
+//                 weight = 11
+//             }
+//             else {
+//                 weight = parseInt(vals[i])
+//             }
+//             var card = { Value: vals[i], Suit: suits[j], Weight: weight };
+//             deck.push(card);
+//         }
+//     }
+//     return console.log(deck.length),
+//     console.log(deck);
+// }
 
-//credit: fisher-yates shuffle method
-function shuffleDeck(deck) {
-    var i = 0, j = 0, temp = null
+// //credit: fisher-yates shuffle method
+// function shuffleDeck(deck) {
+//     var i = 0, j = 0, temp = null
 
-    for (i = deck.length - 1; i > 0; i -= 1) {
-        j = Math.floor(Math.random() * (i + 1))
-        temp = deck[i]
-        deck[i] = deck[j]
-        deck[j] = temp
-    }
-    return console.log(deck.length),
-    console.log(deck);
-}
-
+//     for (i = deck.length - 1; i > 0; i -= 1) {
+//         j = Math.floor(Math.random() * (i + 1))
+//         temp = deck[i]
+//         deck[i] = deck[j]
+//         deck[j] = temp
+//     }
+//     return console.log(deck.length),
+//     console.log(deck);
+// }
 
 function drawCards() {
         var docUrl = "https://deckofcardsapi.com/api/deck/new/draw/?count=2"
-    
         $.ajax({
             url: docUrl,
             method: "GET"
         }).then(function(data) {
-            console.log(data)
+            console.log(data);
+            hand = [];
+            var cardOne = {
+                    ID: 1,
+                    code: data.cards[0].code,
+                    suit: data.cards[0].suit,
+                    value: data.cards[0].value,
+                    imgUrl: drawCards.cards[0].image
+
+                };
+            var cardTwo = 
+                {
+                    ID: 1,
+                    code: data.cards[1].code,
+                    suit: data.cards[1].suit,
+                    value: data.cards[1].value,
+                    imgUrl: drawCards.cards[1].image
+                }
+            ;
+            hand.push(cardOne, cardTwo);
         })
 }
 
 drawCards();
-
 
 //currently a hard coded array under variables but will need a function where there are actual players
 // // function addPlayers(amount) {
@@ -132,6 +148,8 @@ function onStart() {
 
     createDeck();
     shuffleDeck(deck);
+
+    drawCards();
 
     // dealHands();
 }
