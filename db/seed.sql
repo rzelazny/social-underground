f6f52a11bbb800cbf5ef93d36c00c0c4c13ddd3c
Ryan Zelazny <RyanZelazny@gmail.com>
2020-12-08 10:20:48 -0500

    updated seed data

--- db/seed.sql
+++ db/seed.sql
@@ -2,7 +2,7 @@ INSERT INTO user_login (email, password, createdAt, updatedAt)
 VALUES ("Alice@aliceMail.com", "password", "2020-12-21", "2020-12-21"), ("Bob@aol.com", "password", "2020-12-21", "2020-12-21");
 
 INSERT INTO user_stats (login_id, display_name, wins, losses)
-VALUES (1, "Alice", 10, 3), (2, "Bob" 5, 10);
+VALUES (1, "Alice", 10, 3), (2, "Bob", 5, 10);
 
 INSERT INTO gaming_tables (game, game_started, user1)
 VALUES ("Just Chatting", false, 1), ("Black Jack", true, 2);
