d93202be953980778cc2fb48a18fd73c295a7343
J Rhee <jayrhee0514@gmail.com>
2020-12-11 14:33:14 -0500

    fixed up html file and added new variables to memebers.js

--- public/assets/js/members.js
+++ public/assets/js/members.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  
+  //variables
+  var userName = document.getElementsByClassName("card-text-user")
+  var userBio = document.getElementsByClassName("card-text-bio")
+  var userStat = document.getElementsByClassName("card-text-w/l")
+
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
   $.get("/api/user_data").then(function(data) {
@@ -7,5 +13,10 @@ $(document).ready(function() {
 
   $.get("/api/user_stat").then(function(data) {
     $(".member-name").text(data.email);
-  })
+  });
+
+
+  function proFile() {
+
+  }
 });
