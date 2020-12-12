24d4b4af9fcf4f6689bcbeb5d7e41db876d6fb99
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-12 17:44:43 -0500

    fixed bugs. the multiplayer version is done in its prompt form

--- games/rps/multiplayer/rps.js
+++ games/rps/multiplayer/rps.js
@@ -29,22 +29,42 @@ for (var i = 0; i < 10; i++) {
         if ((player1Guess === "r" && player2Guess === "s") ||
             (player1Guess === "s" && player2Guess === "p") ||
             (player1Guess === "p" && player2Guess === "r")) {
-            player1wins++ && player2losses++;
+            player1wins++;
+            console.log("player 1 wins, increment 1 wins")
+            console.log("player 1 wins: " + player1wins);
+            player2losses++;
+            console.log("player 1 wins, increment 2 losses")
+            console.log("player 2 losses: " + player2losses);
             alert(`Player 1 wins this round.`);
             alert (`Current stats: 
             Player 1: ${player1wins} wins. ${player1ties} ties. ${player1losses} losses.
             Player 2: ${player2wins} wins. ${player2ties} ties. ${player2losses} losses.`)
         } else if (player1Guess === player2Guess) {
-            player1ties++ && player2ties++;
+            player1ties++;
+            console.log("players tied, increment 1 ties")
+            console.log("player 1 ties: " + player1ties);
+            player2ties++;
+            console.log("players tied, increment 2 ties")
+            console.log("player 2 ties: " + player2ties);
             alert("You've tied.");
             alert (`Current stats: 
             Player 1: ${player1wins} wins. ${player1ties} ties. ${player1losses} losses.
             Player 2: ${player2wins} wins. ${player2ties} ties. ${player2losses} losses.`)
-        } else {
+        } else if ((player2Guess === "r" && player1Guess === "s") ||
+        (player2Guess === "s" && player1Guess === "p") ||
+        (player2Guess === "p" && player1Guess === "r")){
+            player2wins++;
+            console.log("player 2 wins, increment 2 wins")
+            console.log("player 2 wins: " + player2wins);
+            player1losses++;
+            console.log("player 2 wins, increment 1 losses")
+            console.log("player 1 losses: " + player1losses);
             alert(`Player 2 wins this round.`);
             alert (`Current stats: 
             Player 1: ${player1wins} wins. ${player1ties} ties. ${player1losses} losses.
             Player 2: ${player2wins} wins. ${player2ties} ties. ${player2losses} losses.`)
+        } else {
+            console.log("else statement being reached")
         }
     }
 }
@@ -52,8 +72,10 @@ for (var i = 0; i < 10; i++) {
 // figure out who won
 if (player1wins > player2wins) {
     winner = "Player 1";
+    console.log("player 1 is the winner")
 } else {
     winner = "Player 2";
+    console.log("player 2 is the winner")
 }
 
 
