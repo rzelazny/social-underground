6b50e68dcc1487e49115d3af9cbc683235f71490
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-13 23:01:53 -0500

    finished 3 player house logic

--- games/blackjack/multiplayer - wip/blackjack.js
+++ games/blackjack/multiplayer - wip/blackjack.js
@@ -1284,6 +1284,36 @@ function houseLogic() {
             // endRound();
         }
         // house will stand if ... //
+        // p1 points > house BUT < 22
+        else if (playerArray[1].Bust === false && playerArray[1].Points > playerArray[0].Points) {
+            console.log("house will hit")
+        }
+        // p2 points > house BUT < 22
+        else if (playerArray[2].Bust === false && playerArray[2].Points > playerArray[0].Points) {
+            console.log("house will hit")
+        }
+        // p3 points > house BUT < 22
+        else if (playerArray[3].Bust === false && playerArray[3].Points > playerArray[0].Points) {
+            console.log("house will hit")
+        }
+        // player 1 tied && points < 17
+        else if (playerArray[1].Points === playerArray[0].Points && playerArray[0].Points < 17) {
+            console.log("house will hit")
+        }
+        // player 2 tied && points < 17
+        else if (playerArray[2].Points === playerArray[0].Points && playerArray[0].Points < 17) {
+            console.log("house will hit")
+        }
+        // player 3 tied && points < 17
+        else if (playerArray[3].Points === playerArray[0].Points && playerArray[0].Points < 17) {
+            console.log("house will hit")
+        }
+        // stand //
+        else {
+            playerArray[0].Stand = true;
+            console.log("we will run end round");
+            // endRound();
+        }
 
 
     }
