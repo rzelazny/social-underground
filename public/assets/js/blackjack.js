d30f09b38941435b68ac85216c181ed95bfb9a41
Brianna Hebeler <briannahebeler@gmail.com>
2020-12-14 22:59:04 -0500

    created elements on html to dynamically append on the call of the end round function so that the js doesnt send alerts anymore

--- public/assets/js/blackjack.js
+++ public/assets/js/blackjack.js
@@ -8,11 +8,7 @@
 ///////////////////////////////////////////////
 var players =  document.querySelector('#players');
 var directions = document.querySelector('#directions');
-var playerBusted = document.querySelector('#playerBusted');
-var houseBusted = document.querySelector('#houseBusted');
-var playerTied = document.querySelector('#playerTied');
-var playerWon = document.querySelector('#playerWon');
-var playerLost = document.querySelector('#playerLost');
+var endRoundDiv = document.querySelector('#endRoundDiv');
 
 
 var startBtn = document.querySelector('#start');
@@ -27,6 +23,14 @@ var divPlayer = null;
 var divHand = null;
 var divPoints = null;
 var divScore = null;
+var pointDisplayHouse = null;
+var pointDisplayPlayer = null;
+var winnerDisplay = null;
+var winnerDisplayHouse = null;
+var winnerDisplayPlayer = null;
+var scoresDisplay = null;
+var scoresDisplayHouse = null;
+var scoresDisplayPlayer = null;
 
 
 
@@ -54,21 +58,10 @@ function displayBtns() {
     if (startBtn.style.display === 'block') {
         startBtn.style.display = 'none'
     }
-    if (playerBusted.style.display === 'block') {
-        playerBusted.style.display = 'none'
+    if (endRoundDiv.style.display === 'block') {
+        endRoundDiv.style.display = 'none'
     }
-    if (houseBusted.style.display === 'block') {
-        houseBusted.style.display = 'none'
-    }
-    if (playerTied.style.display === 'block') {
-        playerTied.style.display = 'none'
-    }
-    if (playerLost.style.display === 'block') {
-        playerLost.style.display = 'none'
-    }
-    if (playerWon.style.display === 'block') {
-        playerWon.style.display = 'none'
-    }
+    
     if (directions.style.display === 'block') {
         directions.style.display = 'none'
     }
@@ -232,6 +225,7 @@ function onRestart() {
     playerArray[1].Stand = false;
     // will clear everything on the gameboard //
     players.innerHTML = '';
+    endRoundDiv.innerHTML = '';
     // reset i back to 0 for drawCards function //
     i = 0;
     // makes sure the buttons are displayed in case the round ended previously and buttons were hidden //
@@ -457,9 +451,17 @@ function endRound() {
     console.log("======ending round========")
     // the users will get an alert that the game is over //
     // display points from round to user //
-    alert(`The round is over
-    ${playerArray[0].Name}: ${playerArray[0].Points} points
-    ${playerArray[1].Name}: ${playerArray[1].Points} points`)
+    endRoundDiv.style.display = "block";
+    endRoundDiv.innerHTML = `The round is over`
+    pointDisplayHouse = document.createElement("div");
+    pointDisplayPlayer = document.createElement("div");
+    pointDisplayHouse.innerHTML = `${playerArray[0].Name}: ${playerArray[0].Points} points `
+    pointDisplayPlayer.innerHTML = `${playerArray[1].Name}: ${playerArray[1].Points} points`
+    endRoundDiv.appendChild(pointDisplayHouse);
+    endRoundDiv.appendChild(pointDisplayPlayer);
+    // alert(`The round is over
+    // ${playerArray[0].Name}: ${playerArray[0].Points} points
+    // ${playerArray[1].Name}: ${playerArray[1].Points} points`)
     
     // if the players tie //
     if (playerArray[0].Points === playerArray[1].Points) {
@@ -468,10 +470,22 @@ function endRound() {
         // playerArray[0].Score = playerArray[0].Score + 1;
         // playerArray[1].Score = playerArray[1].Score + 1;
         // alert the users of current scores //
-        alert(`You tied, no players scores were increased.
-        Current Scores: 
-        ${playerArray[0].Name}: ${playerArray[0].Score}
-        ${playerArray[1].Name}: ${playerArray[1].Score}`)
+        // alert(`You tied, no players scores were increased.
+        // Current Scores: 
+        // ${playerArray[0].Name}: ${playerArray[0].Score}
+        // ${playerArray[1].Name}: ${playerArray[1].Score}`)
+        winnerDisplay = document.createElement("div");
+        scoresDisplay = document.createElement("div");
+        scoresDisplayHouse = document.createElement("div");
+        scoresDisplayPlayer = document.createElement("div");
+        winnerDisplay.innerHTML = `You tied, no players scores were increased.`
+        scoresDisplay.innerHTML = `Current Scores: `
+        scoresDisplayHouse.innerHTML = `${playerArray[0].Name}: ${playerArray[0].Score}`
+        scoresDisplayPlayer.innerHTML = `${playerArray[1].Name}: ${playerArray[1].Score}`
+        endRoundDiv.appendChild(winnerDisplay);
+        endRoundDiv.appendChild(scoresDisplay);
+        endRoundDiv.appendChild(scoresDisplayHouse);
+        endRoundDiv.appendChild(scoresDisplayPlayer);
         // // update scores on html //
         // divScore.innerHTML = `Score: ${playerArray[0].Score} `;
         // divScore.innerHTML = `Score: ${playerArray[1].Score} `;
@@ -483,10 +497,22 @@ function endRound() {
         // increase House points by 2 //
         playerArray[0].Score = playerArray[0].Score + 1;
         // alert the users of current scores //
-        alert(`House wins
-        Current Scores: 
-        ${playerArray[0].Name}: ${playerArray[0].Score}
-        ${playerArray[1].Name}: ${playerArray[1].Score}`) 
+        winnerDisplay = document.createElement("div");
+        scoresDisplay = document.createElement("div");
+        scoresDisplayHouse = document.createElement("div");
+        scoresDisplayPlayer = document.createElement("div");
+        winnerDisplay.innerHTML = `House wins.`
+        scoresDisplay.innerHTML = `Current Scores: `
+        scoresDisplayHouse.innerHTML = `${playerArray[0].Name}: ${playerArray[0].Score}`
+        scoresDisplayPlayer.innerHTML = `${playerArray[1].Name}: ${playerArray[1].Score}`
+        endRoundDiv.appendChild(winnerDisplay);
+        endRoundDiv.appendChild(scoresDisplay);
+        endRoundDiv.appendChild(scoresDisplayHouse);
+        endRoundDiv.appendChild(scoresDisplayPlayer);
+        // alert(`House wins
+        // Current Scores: 
+        // ${playerArray[0].Name}: ${playerArray[0].Score}
+        // ${playerArray[1].Name}: ${playerArray[1].Score}`) 
         scoreHouse = document.querySelector("#scoreHouse");
         scoreHouse.innerHTML = `Score: ${playerArray[0].Score} `;
 
@@ -498,10 +524,22 @@ function endRound() {
         // increase player1 points by 2 //
         playerArray[1].Score = playerArray[1].Score + 1;
         // alert the users of current scores //
-        alert(`Player1 wins
-        Current Scores: 
-        ${playerArray[0].Name}: ${playerArray[0].Score}
-        ${playerArray[1].Name}: ${playerArray[1].Score}`) 
+        winnerDisplay = document.createElement("div");
+        scoresDisplay = document.createElement("div");
+        scoresDisplayHouse = document.createElement("div");
+        scoresDisplayPlayer = document.createElement("div");
+        winnerDisplay.innerHTML = `Player1 wins.`
+        scoresDisplay.innerHTML = `Current Scores: `
+        scoresDisplayHouse.innerHTML = `${playerArray[0].Name}: ${playerArray[0].Score}`
+        scoresDisplayPlayer.innerHTML = `${playerArray[1].Name}: ${playerArray[1].Score}`
+        endRoundDiv.appendChild(winnerDisplay);
+        endRoundDiv.appendChild(scoresDisplay);
+        endRoundDiv.appendChild(scoresDisplayHouse);
+        endRoundDiv.appendChild(scoresDisplayPlayer);
+        // alert(`Player1 wins
+        // Current Scores: 
+        // ${playerArray[0].Name}: ${playerArray[0].Score}
+        // ${playerArray[1].Name}: ${playerArray[1].Score}`) 
         scoreHouse = document.querySelector("#scoreHouse");
         scoreHouse.innerHTML = `Score: ${playerArray[0].Score} `
         scorePlayer1 = document.querySelector("#scorePlayer1");
